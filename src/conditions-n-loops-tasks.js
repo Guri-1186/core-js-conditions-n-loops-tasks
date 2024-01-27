18cd8bc1bd10899660eb667e59bd26625dc9a6d0
Guranda Jikia <jikiaguranda@gmail.com>
2024-01-27 17:08:12 +0400

    fix:made some changes

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -472,8 +472,23 @@ function rotateMatrix(matrix) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('task isnt implemented');
+function sortByAsc(arr) {
+  const newArr = arr.slice(); // Create a copy of the input array
+
+  for (let i = 1; i < newArr.length; i += 1) {
+    const elem = newArr[i];
+    let j = i;
+    i -= 1;
+
+    while (j >= 0 && newArr[j] > elem) {
+      newArr[j + 1] = newArr[j];
+      j -= 1;
+    }
+
+    newArr[j + 1] = elem;
+  }
+
+  return newArr;
 }
 
 /**
